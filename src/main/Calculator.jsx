91a19814b503efbc90043f1e47418ef2cebc0194
emilysk49 <emilytti@me.com>
2026-02-25 17:18:05 -0300

    feat: commit inicial

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './Calculator.css'
+
+export default class Calculator extends Component {
+    render () {
+        return (
+            // className é mesma coisa que class
+            <div className='calculator'>
+
+            </div>
+        )
+    }
+}
